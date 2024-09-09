58c6437ed1ae695fdafe9443fd234b3d9cb06eed
Gabriel <g.chaves0110@gmail.com>
2024-09-08 21:57:14 -0300

    feat: Create NavBar Component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import { AppBar, Grid, MenuItem, styled } from '@mui/material';
+import { IconButton, Toolbar, Typography } from '@mui/material';
+import StyledButton from './styledButton';
+
+const NavBar = () => {
+
+const StyledToolBar = styled(Toolbar)(({theme}) => ({
+    display: "flex",
+    justifyContent: "space-evenly",
+}));
+  return (
+    <AppBar position="absolute" color="secondary">
+        <StyledToolBar>
+            <MenuItem>About</MenuItem>
+            <MenuItem>Projects</MenuItem>
+            <StyledButton>Clicaaaaaaaaaaa</StyledButton>
+        </StyledToolBar>
+    </AppBar>
+  );
+}
+
+export default NavBar;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import NavBar from "../../components/NavBar";
 import Hero from "./Sections/Hero/Hero";
 
 const Home = () => {
   return (
     <>
+      <NavBar />
       <Hero />
     </> 
   );
